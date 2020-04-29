73f86732aaa058b9fc4a32fdc7e275d9a4868e38
unknown <dgloria.web@gmail.com>
2020-04-29 06:21:04 +0100

    created movie detail page

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    {{ $route.params.id }}
-  </div>
+<h1>
+    {{movie.title}}
+    </h1> 
+     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      movie: {},
+    }
+  },
+  created: function() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=ec5dc58dd49114aa47aebd1659fef849`
+        )
+        const movie = await res.json()
+        this.movie = movie
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
